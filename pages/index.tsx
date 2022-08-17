566682af288996116be50ddb0422b5263324ca41
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-08-16 22:49:44 -0400

    Init Portfolio

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,9 +33,9 @@ const Home: NextPage = () => {
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
-          <a
+          <a 
             href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
+            className='mr-42'
           >
             <h2>Examples &rarr;</h2>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
