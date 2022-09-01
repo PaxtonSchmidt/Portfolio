f006b91710d6c3f1bc11ba3afda36b37bfc60f91
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-08-31 23:01:56 -0400

    toml

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,8 +12,8 @@ const Home: NextPage = () => {
         <title>Paxton Schmidt, Developer</title>
         <meta name="description" content="A portfolio of web development works by Paxton Schmidt" />
         <link rel="icon" href="/favicon.ico" />
-        <style>@import url('https://fonts.googleapis.com/css2?family=Fredoka:wdth,wght@75,517;100,300;100,400;100,500;100,600;100,700&family=IM+Fell+DW+Pica&family=Montserrat:wght@800&display=swap')</style>
-        <style>@import url('https://fonts.googleapis.com/css2?family=Fredoka:wdth,wght@75,517;100,300;100,400;100,500;100,600;100,700&family=IM+Fell+DW+Pica&family=Lato&display=swap')</style>
+        <style>@import url("https://fonts.googleapis.com/css2?family=Fredoka:wdth,wght@75,517;100,300;100,400;100,500;100,600;100,700&family=IM+Fell+DW+Pica&family=Montserrat:wght@800&display=swap")</style>
+        <style>@import url("https://fonts.googleapis.com/css2?family=Fredoka:wdth,wght@75,517;100,300;100,400;100,500;100,600;100,700&family=IM+Fell+DW+Pica&family=Lato&display=swap")</style>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
       </Head>
 
