5bc49b8e20d541bb64294e308c34eace6e9e7253
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-09-01 11:06:52 -0400

    oops, forgot the icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,7 +11,7 @@ const Home: NextPage = () => {
       <Head>
         <title>Paxton Schmidt, Developer</title>
         <meta name="description" content="A portfolio of web development works by Paxton Schmidt" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href='/favicon.svg' />
         <style>@import url("https://fonts.googleapis.com/css2?family=Fredoka:wdth,wght@75,517;100,300;100,400;100,500;100,600;100,700&family=IM+Fell+DW+Pica&family=Montserrat:wght@800&display=swap")</style>
         <style>@import url("https://fonts.googleapis.com/css2?family=Fredoka:wdth,wght@75,517;100,300;100,400;100,500;100,600;100,700&family=IM+Fell+DW+Pica&family=Lato&display=swap")</style>
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
