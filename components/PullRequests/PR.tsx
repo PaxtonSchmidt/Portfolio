980677ddc127fbcc3bcdf633bde5e67847226c5f
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-09-06 19:50:32 -0400

    Work with github api

--- components/PullRequests/PR.tsx
+++ components/PullRequests/PR.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 interface Props{
 
@@ -6,6 +6,22 @@ interface Props{
 
 //a component that neatly displays PRs?
 export const PR: React.FC<Props> = () => {
+
+
+
+  async function getRepos(){
+
+
+  }
+
+  useEffect(()=>{
+    const init = {
+      method: 'GET',
+      mode: 'cors',
+    }
+    let response = fetch('https://api.github.com/users/');
+  }, [])
+
   return (
     <div>
       Test
