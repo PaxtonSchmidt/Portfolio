e4bcf57df8f6a6e927936d645fbc59fd5e1e743b
PaxtonSchmidt <67126427+PaxtonSchmidt@users.noreply.github.com>
2022-09-14 09:07:22 -0400

    Changed details button

--- components/Projects/ProjectItem.tsx
+++ components/Projects/ProjectItem.tsx
@@ -30,12 +30,19 @@ export const ProjectItem: React.FC<props> = ({ data }) => {
             <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.012 8.012 0 0 0 16 8c0-4.42-3.58-8-8-8z"/>
           </svg>
           </ProjectItemNavButton>
+
+          <button onClick={()=>setIsOpen(true)} className='flex font-bold font-serif mr-2'>
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="20" fill="currentColor" className="bi bi-aspect-ratio text-lightBlue drop-shadow" viewBox="0 0 16 16">
+            <path d="M0 3.5A1.5 1.5 0 0 1 1.5 2h13A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-13A1.5 1.5 0 0 1 0 12.5v-9zM1.5 3a.5.5 0 0 0-.5.5v9a.5.5 0 0 0 .5.5h13a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-13z"/>
+            <path d="M2 4.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 0 1H3v2.5a.5.5 0 0 1-1 0v-3zm12 7a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1 0-1H13V8.5a.5.5 0 0 1 1 0v3z"/>
+          </svg>
+            <span className='flex text-lightBlue drop-shadow'>
+              {`\u00a0Details`}
+            </span>
+          </button>
         </div>
         <img src={`${data.img}`} className='w-80 drop-shadow mx-auto lg:ml-0'/>
 
-        <button onClick={()=>setIsOpen(true)} className='h-[40px] mx-auto lg:ml-0 bottom-0 mt-2 w-fit px-6 rounded-3xl bg-black text-white shadow-[0px_0px_3px_rgba(0,0,0,0.3)] shadow-white'>
-          Details
-        </button>
       </figure>
 
       <div className='flex flex-col mx-auto lg:justify-start content-center pt-2 lg:pt-12 lg:pl-6 lg:row-span-2 lg:col-span-4 xl:col-span-3 max-w-[500px]'>
